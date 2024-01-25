c86dbeff5b7aa6d18a118ba8a4fc40ad1d89d4b0
Lienae <ktx408216@gmail.com>
2024-01-25 14:32:10 +0900

    0125th
    to t h

--- 0111th/0725/src/main/java/idusw/leafton/model/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package idusw.leafton.model.service;
-
-import idusw.leafton.model.DTO.*;
-
-public interface OrderService {
-    OrderDTO addOrder(OrderDTO orderDTO);
-
-    OrderItemDTO addOrderItem(OrderDTO orderDTO, ProductDTO productDTO, int count);
-}
