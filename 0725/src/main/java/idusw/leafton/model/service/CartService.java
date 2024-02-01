9682f92c0aa210c119e87df6f080312002d5d46a
Lienae <ktx408216@gmail.com>
2024-02-01 16:19:57 +0900

    0201th
    0201 fix

--- 0725/src/main/java/idusw/leafton/model/service/CartService.java
+++
@@ -1,8 +0,0 @@
-package idusw.leafton.model.service;
-
-import idusw.leafton.model.DTO.MemberDTO;
-import idusw.leafton.model.DTO.ProductDTO;
-
-public interface CartService {
-    void addCart(MemberDTO member, ProductDTO product, int count);
-}
